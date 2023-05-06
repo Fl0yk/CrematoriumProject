f3cbbd0e27e94df03f32a88811eb53b44724f5a2
Дима Косач <kosach.dmitriy@gmail.com>
2023-05-06 19:28:35 +0300

    Start users page

--- Crematorium/Crematorium.UI/obj/Debug/net6.0-windows/Pages/LoginPage.g.i.cs
+++ Crematorium/Crematorium.UI/obj/Debug/net6.0-windows/Pages/LoginPage.g.i.cs
@@ -54,7 +54,7 @@ namespace Crematorium.UI.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Crematorium.UI;component/pages/loginpage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Crematorium.UI;V1.0.0.0;component/pages/loginpage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Pages\LoginPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
